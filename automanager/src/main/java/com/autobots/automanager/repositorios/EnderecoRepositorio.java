25c6cb92423940ff0a06b502a80f92fac713026e
LucasVinicius32 <Lucasviniciuswinner2012@gmail.com>
2023-06-16 19:00:54 -0300

    atividade 3

--- automanager/src/main/java/com/autobots/automanager/repositorios/EnderecoRepositorio.java
+++ automanager/src/main/java/com/autobots/automanager/repositorios/EnderecoRepositorio.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.autobots.automanager.entidades.Endereco;
 
 public interface EnderecoRepositorio extends JpaRepository<Endereco, Long> {
-
-   
 }
